26ad544ec0c5c57c2c5d2f86cebe08a06821e8ee
Manalang1 <Manal.angari@gmail.com>
2022-03-09 10:28:53 +0300

    Initial Commit

---
+++ MobeenApplication/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MobeenApplication
+//
+//  Created by Manal Alangari on 09/03/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
